84b7eb5134fcac783e8b3eb5cb86b2667668adc2
David Hanak <David Hanak@3be10270-9b13-ef40-826a-d1ec77913d82>
2008-09-14 18:42:09 +0000

    Less rigorous check of favicon.ico mime type: refuse text/*, accept everything else.
    git-svn-id: file:///windows/SVNRepo/identfavicon/trunk@17 3be10270-9b13-ef40-826a-d1ec77913d82

--- chrome/content/identfavicon.js
+++ chrome/content/identfavicon.js
@@ -146,8 +146,8 @@ var identFavIcon = {
     IdentFavicon generator
 
     @author  David Hanak
-    @version 0.1.8
-    @date    August 30, 2008
+    @version 0.1.9
+    @date    September 14, 2008
     */
     mIOS: Components.classes["@mozilla.org/network/io-service;1"]
             .getService(Components.interfaces.nsIIOService),
@@ -174,7 +174,7 @@ var identFavIcon = {
                     } else {
                         //alert('Type: ' + aChannel.contentType + ', length: ' + aChannel.contentLength);
                         if (!aChannel.requestSucceeded ||
-                            !aChannel.contentType.match('^image/.+$') ||
+                            aChannel.contentType.match('^text/.+$') ||
                             aChannel.contentLength == 0)
                         {
                             gBrowser.mFaviconService.addFailedFavicon(aChannel.URI);
